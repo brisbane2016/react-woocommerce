7b57781dbb97181c8bca2de6c8131fa4bb146b5d
lixiaotao <lixiaotao@hotmail.com>
2019-09-13 14:44:48 +1200

    first start

---
+++ components/Products.js
@@ -0,0 +1,8 @@
+const Product = (props) =>(
+
+    <div>
+        products
+    </div>
+)
+
+export default Product;
---
+++ wooConfig.js
@@ -0,0 +1,7 @@
+const wooConfig = {
+	siteUrl: 'http://qiqiu.yippee.co.nz',
+	consumerKey: 'ck_3a4d1e8af2e0818369c5ab7b830bcee7d66423d6',
+	consumerSecret: 'cs_04eb2498e8e2e34b07746884088d5c1e3761e26b'
+};
+
+module.exports = wooConfig;
